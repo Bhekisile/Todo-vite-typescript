55a4937d3fc3f262ab6fea73a5ad90a6d545079c
Bhekisile <bhekisilejozi@gmail.com>
2025-10-30 13:16:58 +0200

    Removed not used variables

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import {
   Select,
--- src/components/userProfile/userProfile.tsx
+++ src/components/userProfile/userProfile.tsx
@@ -1,6 +1,6 @@
 import type { FC, ReactElement } from "react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
 
 export const UserProfile: FC<{firstName: string}> = (props): ReactElement => {
